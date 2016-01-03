23d62572b3214268be198a2139491a2525eefa27
heng <821600682@qq.com>
2016-01-04 03:39:34 +0800

    build.sh、修改

--- src/main/resources/build.sh
+++ src/main/resources/build.sh
@@ -1,6 +1,5 @@
-
-CURRENT_FOLDER=`pwd`
-echo "$CURRENT_FOLDER"
+#CURRENT_FOLDER=`pwd`
+#echo "CURRENT_FOLDER=[$CURRENT_FOLDER]"
 
 #源码存放的路径
 SRC_FOLDER='/data/mvn/googlesearch'
@@ -10,41 +9,33 @@ git pull
 
 mvn clean package
 
-cd target
-TARGET_FOLDER=`pwd`
+TARGET_FOLDER="$SRC_FOLDER/target"
 
 
-#tomcat存放的路径
-TOMCAT_FOLDER='/home/ubuntu/data/tomcat/googlesearch.com'
+#编译结果存放目录
+BUILD_FOLDER="$SRC_FOLDER/build";
+
+#清理准备环境
+if [ ! -d "$BUILD_FOLDER" ]; then
+  	mkdir "$BUILD_FOLDER"
+else  
+  #备份
+	BUILD_FOLDER_BACKUP="$BUILD_FOLDER-`date+%Y%m%d-%H%M%S`"
+	mv "$BUILD_FOLDER" "$BUILD_FOLDER_BACKUP"
+	mkdir "$BUILD_FOLDER"
+fi
 
 
-#先关掉对应的tomcat应用进程
-echo '关闭进程：\n'
-#ps -aux|grep tomcat| grep googlesearch
-#app_pid=$(ps -aux|grep tomcat| grep googlesearch| awk '{print $2}')
-#kill $app_pid
-SHUTDOWN_SH="$tomcat_folder/bin/shutdown.sh"
+#编译结果拷贝过来
+cp -R "$TARGET_FOLDER/." "$BUILD_FOLDER"
+cd "$BUILD_FOLDER"
 
-#执行tomcat的shutdown.sh
-$SHUTDOWN_SH
+#去掉一些
+rm -Rf classes lib generated-sources generated-test-sources maven-archiver maven-status surefire-reports test-classes .war
 
 
-#运行时文件存放 的路径
-APP_RUNTIME_FOLDER='/data/runapp/googlesearch.com'
-#备份
-APP_RUNTIME_FOLDER_BACKUP="$APP_RUNTIME_FOLDER-`date+%Y%m%d-%H%M%S`"
-mv "$APP_RUNTIME_FOLDER" "$APP_RUNTIME_FOLDER_BACKUP"
-
-cp -R "$TARGET_FOLDER" "$APP_RUNTIME_FOLDER"
-cd "$APP_RUNTIME_FOLDER"
-
-rm -Rf classes lib generated-sources generated-test-sources maven-archiver maven-status surefire-reports test-classes
-
-
-STARTUP_SH="$tomcat_folder/bin/startup.sh"
-$STARTUP_SH
-
-
+#回到原来的目录
+#cd "$CURRENT_FOLDER"
 
 
 

317f23a237c54f0ebe27040db78e4ec28ef2ecfb
heng <821600682@qq.com>
2016-01-09 04:07:57 +0800

    build.sh comment change to english

--- build.sh
+++ build.sh
@@ -1,7 +1,8 @@
+#store current shell pwd folder
 CURRENT_FOLDER=`pwd`
 echo "CURRENT_FOLDER=[$CURRENT_FOLDER]"
 
-#源码存放的路径
+#the source code folder place
 SRC_FOLDER='/data/mvn/googlesearch'
 cd "$SRC_FOLDER"
 
@@ -12,29 +13,29 @@ mvn clean package
 TARGET_FOLDER="$SRC_FOLDER/target"
 
 
-#编译结果存放目录
+#compiled code folder
 BUILD_FOLDER="$SRC_FOLDER/build";
  
-#清理准备环境
+#clean builde folder
 if [ ! -d "$BUILD_FOLDER" ]; then
   	mkdir "$BUILD_FOLDER"
 else  
-  #备份
+  #back up
 	BUILD_FOLDER_BACKUP="$BUILD_FOLDER-$(date +%Y%m%d-%H%M%S)"
 	mv "$BUILD_FOLDER" "$BUILD_FOLDER_BACKUP"
 	mkdir "$BUILD_FOLDER"
 fi
 
 
-#编译结果拷贝过来
+#copy the compiled result
 cp -R "$TARGET_FOLDER/." "$BUILD_FOLDER"
 cd "$BUILD_FOLDER"
 
-#去掉一些
+#remove useless file from maven compiled result
 rm -Rf classes lib generated-sources generated-test-sources maven-archiver maven-status surefire-reports test-classes .war
 
 
-#回到原来的目录
+#return to stored current pwd folder 
 cd "$CURRENT_FOLDER"
 
 
